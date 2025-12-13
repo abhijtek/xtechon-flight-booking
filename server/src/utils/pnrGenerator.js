d35ae58320a007c7caf874772131ec64e9ea2a02
abhijtek <singhabhijeet1212@gmail.com>
2025-12-13 16:54:08 +0530

    feat: add pnr generator utility

---
+++ server/src/utils/pnrGenerator.js
@@ -0,0 +1,12 @@
+function generatePNR() {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  let code = "";
+  for (let i = 0; i < 6; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return `PNR-${code}`;
+  // 6 length pnr 36 power 6
+}
+
+module.exports = { generatePNR };
