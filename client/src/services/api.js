2996f65efda8b8cc561c1d66572079b20f51fedb
abhijtek <singhabhijeet1212@gmail.com>
2025-12-16 14:18:31 +0530

    upd: deployment issues, reverting to local running

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://xtechon-flight-booking.onrender.com/api",
+  baseURL: "http://localhost:4000/api",
 });
 
 export const getFlights = ({ page }) =>
